adac5cdb4434e94ad120673223bdf4589f6148a3
420002190 <marcel.marian@ge.com>
2020-11-27 17:18:14 -0500

    add Put/GetFile functionality

--- testGrpcClient.go
+++ testGrpcClient.go
@@ -14,32 +14,34 @@ import (
 	"google.golang.org/grpc"
 )
 
-func runGrpcFileTransfer(client grpcSrv.GrpcServiceClient, filename string) {
+func runGrpcFilePut(client grpcSrv.GrpcServiceClient, filename string) {
 	var chunksCount int = 10
 	var fileChunks []*grpcSrv.FileChunk
-	var arr []byte
-	arr = []byte("Here is a string....")
+	arr := []byte("Here is a string....")
 
 	// Create a random number of random points
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	stream, err := client.TransferFile(ctx)
+	stream, err := client.PutFile(ctx)
 	if err != nil {
-		log.Fatalf("%v.TransferFile(_) = _, %v", client, err)
+		log.Fatalf("%v.PutFile(_) = _, %v", client, err)
 	}
 
+	// Simulate the file reading
 	for i := 0; i < chunksCount; i++ {
 		var req grpcSrv.FileChunk
 		req.Filename = filename
 		req.Packet = arr
-		req.ByteCount = 10
 		if i < 9 {
+			req.ByteCount = 999
 			req.More = true
 		} else {
+			req.ByteCount = 9
 			req.More = false
 		}
 		fileChunks = append(fileChunks, &req)
 	}
+	// Simulate the file sending
 	for _, chunk := range fileChunks {
 		if err := stream.Send(chunk); err != nil {
 			log.Fatalf("%v.Send(%v) = %v", stream, chunk, err)
@@ -49,7 +51,38 @@ func runGrpcFileTransfer(client grpcSrv.GrpcServiceClient, filename string) {
 	if err != nil {
 		log.Fatalf("%v.CloseAndRecv() got error %v, want %v", stream, err, nil)
 	}
-	log.Printf("FileTransfer: filename=\"%s\", bytes = %d", reply.Filename, reply.BytesTransfered)
+	log.Printf("FilePut: filename=\"%s\", bytes = %d", reply.Filename, reply.BytesTransfered)
+}
+
+func runGrpcFileGet(client grpcSrv.GrpcServiceClient, filename string) {
+	var req grpcSrv.FileProgress
+	var fileChunks []*grpcSrv.FileChunk
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	req.Filename = filename
+	stream, err := client.GetFile(ctx, &req)
+	if err != nil {
+		log.Fatalf("%v.GetFile(_) = _, %v", client, err)
+	}
+
+	fileLen := 0
+	for {
+		chunk, err := stream.Recv()
+		if err != nil {
+			log.Println("GetFile Error")
+			break
+		}
+		fileChunks = append(fileChunks, chunk)
+		fileLen += int(chunk.ByteCount)
+		if chunk.More == false {
+			break
+		}
+	}
+	req.BytesTransfered = int32(fileLen)
+	log.Println("GetFile request: filename =", req.Filename, "#bytes =", req.BytesTransfered)
+	// fmt.Printf("len=%d cap=%d %v\n", len(fileChunks), cap(fileChunks), fileChunks)
 }
 
 func initSignalHandle() {
@@ -64,10 +97,10 @@ func initSignalHandle() {
 }
 
 func main() {
+	fmt.Println("gRPC client client application")
 	initSignalHandle()
 
 	// gRPC client initialization
-	fmt.Println("gRPC Client client ...")
 	opts := grpc.WithInsecure()
 	conn, err := grpc.Dial("localhost:50051", opts)
 	if err != nil {
@@ -78,9 +111,8 @@ func main() {
 	stopRequest := grpcSrv.StopRequest{Id: "Stop!"}
 	cli := grpcSrv.NewGrpcServiceClient(conn)
 	var j int32 = 0
+	// gRPC client testing loop
 	for {
-		// gRPC client testing loop
-		runGrpcFileTransfer(cli, "TestFileNmae")
 		if j%2 == 0 {
 			// Sending Start command
 			startResp, err := cli.Start(context.Background(), &startRequest)
@@ -88,6 +120,7 @@ func main() {
 				log.Fatalf("Error when calling Start function: %s", err)
 			}
 			log.Printf("Response: %s", startResp.Result)
+			runGrpcFilePut(cli, "TestFileNamePut")
 		} else {
 			// Sending Stop command
 			stopResp, err := cli.Stop(context.Background(), &stopRequest)
@@ -95,6 +128,7 @@ func main() {
 				log.Fatalf("Error when calling Start function: %s", err)
 			}
 			log.Printf("Response: %s", stopResp.Result)
+			runGrpcFileGet(cli, "TestFileNameGet")
 		}
 		j = j + 1
 		time.Sleep(1 * time.Second)
--- testGrpcServer.go
+++ testGrpcServer.go
@@ -3,6 +3,7 @@ package main
 import (
 	context "context"
 	"fmt"
+	grpcInterface "grpcservice"
 	grpcSrv "grpcservice"
 	"log"
 	"net"
@@ -14,6 +15,7 @@ import (
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"google.golang.org/grpc/reflection"
 
 	"google.golang.org/grpc"
 )
@@ -35,39 +37,86 @@ func initSignalHandle() {
 	}()
 }
 
-func (s *GrpcServiceServer) Start(ctx context.Context, req *grpcSrv.StartRequest) (*grpcSrv.StartResponse, error) {
+func (s *GrpcServiceServer) Start(ctx context.Context, req *grpcInterface.StartRequest) (*grpcInterface.StartResponse, error) {
 	log.Println("Start request processing: ", req.Message)
 	client.Publish("testTopic/1", 0, false, "Testing MQTT: Start")
-	return &grpcSrv.StartResponse{Result: "Start"}, nil
+	return &grpcInterface.StartResponse{Result: "Start"}, nil
 }
 
-func (s *GrpcServiceServer) Stop(ctx context.Context, req *grpcSrv.StopRequest) (*grpcSrv.StopResponse, error) {
+func (s *GrpcServiceServer) Stop(ctx context.Context, req *grpcInterface.StopRequest) (*grpcInterface.StopResponse, error) {
 	log.Println("Stop request processing: ", req.Id)
 	client.Publish("testTopic/1", 0, false, "Testing MQTT: Stop")
-	return &grpcSrv.StopResponse{Result: "Stop"}, nil
+	return &grpcInterface.StopResponse{Result: "Stop"}, nil
 }
 
-func (s *GrpcServiceServer) TransferFile(stream grpcSrv.GrpcService_TransferFileServer) error {
-	var chunkCount int32
-	// var lastChunk *grpcSrv.FileChunk
-	var resp grpcSrv.FileProgress
+func (s *GrpcServiceServer) PutFile(stream grpcInterface.GrpcService_PutFileServer) error {
+	var resp grpcInterface.FileProgress
+	var fileLen int32
 
-	client.Publish("testTopic/1", 0, false, "Testing MQTT: TransferFile")
-	chunkCount = 1
+	// Forward the notification over mqtt
+	client.Publish("testTopic/1", 0, false, "Testing MQTT: PutFile")
+
+	// Receive the file content and append it to a slice
+	fileLen = 0
 	for {
 		chunk, err := stream.Recv()
 		if err != nil {
 			log.Println("Transfer File Error")
 			return err
 		}
-		resp.Filename = chunk.Filename
-		resp.BytesTransfered = chunk.ByteCount
+		fileLen += chunk.ByteCount
 		if chunk.More == false {
-			log.Println("TransferFile request processing: filename =", chunk.Filename, "#bytes =", chunk.ByteCount)
+			resp.Filename = chunk.Filename
+			resp.BytesTransfered = fileLen
+			log.Println("PutFile request processing: filename =", resp.Filename, "#bytes =", resp.BytesTransfered)
 			return stream.SendAndClose(&resp)
 		}
-		chunkCount++
 	}
+}
+
+func (s *GrpcServiceServer) GetFile(req *grpcInterface.FileProgress, stream grpcInterface.GrpcService_GetFileServer) error {
+	var fileChunks []*grpcSrv.FileChunk
+
+	// Simulate the file reading
+	for i := 0; i < 10; i++ {
+		var chunk grpcSrv.FileChunk
+		chunk.Filename = req.Filename
+		chunk.Packet = []byte("Here is another string....")
+		if i < 9 {
+			chunk.ByteCount = int32(len(chunk.Packet))
+			chunk.More = true
+		} else {
+			chunk.ByteCount = 11
+			chunk.More = false
+		}
+		fileChunks = append(fileChunks, &chunk)
+	}
+	// Simulate the file sending
+	fileLen := 0
+	for _, chunk := range fileChunks {
+		if err := stream.Send(chunk); err != nil {
+			log.Fatalf("%v.Send(%v) = %v", stream, chunk, err)
+		}
+		fileLen += int(chunk.ByteCount)
+	}
+	log.Printf("GetFile: filename=\"%s\", bytes = %d", req.Filename, fileLen)
+	return nil
+}
+
+func startGrpcServer() {
+	listener, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	server := GrpcServiceServer{}
+	opts := []grpc.ServerOption{}
+	grpcServer := grpc.NewServer(opts...)
+	grpcInterface.RegisterGrpcServiceServer(grpcServer, &server)
+
+	reflection.Register(grpcServer)
+
+	fmt.Println("gRPC listener")
+	grpcServer.Serve(listener)
 }
 
 func mqttConnect(clientId string, uri *url.URL) mqtt.Client {
@@ -91,24 +140,13 @@ func mqttCreateClientOptions(clientId string, uri *url.URL) *mqtt.ClientOptions 
 
 func main() {
 	initSignalHandle()
-	fmt.Println("gRPC server")
+	fmt.Println("gRPC server application")
 
 	uri, err := url.Parse("mqtt://172.18.0.2:1883/testTopic")
+	if err != nil {
+		log.Fatalf("failed to parse url: %v", err)
+	}
 	client = mqttConnect("pub", uri)
 
-	client.Publish("testTopic/1", 0, false, "Testing MQTT")
-
-	// gRPC server initialization
-	listener, err := net.Listen("tcp", "localhost:50051")
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	server := GrpcServiceServer{}
-	opts := []grpc.ServerOption{}
-	grpcServer := grpc.NewServer(opts...)
-	grpcSrv.RegisterGrpcServiceServer(grpcServer, &server)
-	fmt.Println("gRPC listener")
-	grpcServer.Serve(listener)
-
-	fmt.Printf("Terminating\n")
+	startGrpcServer()
 }
